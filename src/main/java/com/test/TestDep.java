031d826c2e3bf1c85d0de20d0ecc9ee9083b9b30
zhangyanlong1 <a942530681@qq.com>
2019-12-04 19:06:23 +0800

    pri冲突

--- src/main/java/com/test/TestDep.java
+++ src/main/java/com/test/TestDep.java
@@ -1,7 +1,12 @@
 package com.test;
 
 public class TestDep {
-
+/**
+ * 
+ * 2222pro
+ * 
+ * @param args
+ */
 	public static void main(String[] args) {
 		//Project  第一个
 		System.out.println("1");
